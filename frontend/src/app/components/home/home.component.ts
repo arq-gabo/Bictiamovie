fda02958cae6d0011fefbcbfae9d85738e528c1e
JairSim_777 <jsvingenielectro@gmail.com>
2020-04-26 20:48:35 -0500

    pelicula revision 1

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,23 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
-
-=======
 import { MoviedbService } from '../../services/moviedb.service';
->>>>>>> sim
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-<<<<<<< HEAD
-export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-=======
 export class HomeComponent {
 
   nuevasPeliculas: any[] = [];
@@ -38,5 +25,4 @@ export class HomeComponent {
   }
 
 
->>>>>>> sim
 }
