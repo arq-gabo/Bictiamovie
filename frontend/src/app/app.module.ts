54c9d0c18f9b73430dd4789f2ac4ade0ad122503
JairSim_777 <jsvingenielectro@gmail.com>
2020-04-26 22:15:00 -0500

    pelicula revision 2

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,57 +1,54 @@
-<<<<<<< HEAD
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule} from '@angular/router';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http'
-
-//components
-import { AppComponent } from './app.component';
-import { NavBarComponent } from './components/nav-bar/nav-bar.component';
-import { JumbotronComponent } from './components/jumbotron/jumbotron.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { SignUpComponent } from './components/sign-up/sign-up.component';
-import { SignInComponent } from './components/sign-in/sign-in.component';
-import { HomeComponent } from './components/home/home.component';
-import { AboutUsComponent } from './components/about-us/about-us.component';
-import { TermsComponent } from './components/terms/terms.component';
-import { SubaccountComponent } from './components/subaccount/subaccount.component';
-
-//guard
-import { UserGuard } from './user.guard';
-import { TokenInterceptorService } from './services/token-interceptor.service'
-
-const routes : Routes = [
-  { path: '', component: HomeComponent},
-  { path: 'signin', component: SignInComponent },
-  { path: 'signup', component: SignUpComponent},
-  { path: 'aboutus', component: AboutUsComponent},
-  { path: 'terms', component: TermsComponent},
-  { path: 'subaccount', component: SubaccountComponent, canActivate: [UserGuard]}
-];
-=======
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { FormsModule } from "@angular/forms";
+import {
+  HttpClientModule,
+  HTTP_INTERCEPTORS,
+  HttpClientJsonpModule,
+} from "@angular/common/http";
 
-import { RouterModule } from "@angular/router";
-import { ROUTES } from "./app.routes";
-
-import { HttpClientModule, HttpClientJsonpModule } from "@angular/common/http";
-
+//components  --
 import { AppComponent } from "./app.component";
+import { NavBarComponent } from "./components/nav-bar/nav-bar.component";
+import { JumbotronComponent } from "./components/jumbotron/jumbotron.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { SignUpComponent } from "./components/sign-up/sign-up.component";
+import { SignInComponent } from "./components/sign-in/sign-in.component";
 import { HomeComponent } from "./components/home/home.component";
+import { AboutUsComponent } from "./components/about-us/about-us.component";
+import { TermsComponent } from "./components/terms/terms.component";
+import { SubaccountComponent } from "./components/subaccount/subaccount.component";
+
 import { SearchComponent } from "./components/search/search.component";
 import { LoadingComponent } from "./components/shared/loading/loading.component";
 import { PeliculaComponent } from "./components/pelicula/pelicula.component";
 import { TarjetasComponent } from "./components/tarjetas/tarjetas.component";
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { PeliculaImagenPipe } from './pipes/pelicula-imagen.pipe';
->>>>>>> sim
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { PeliculaImagenPipe } from "./pipes/pelicula-imagen.pipe";
+
+//guard
+import { UserGuard } from "./user.guard";
+import { TokenInterceptorService } from "./services/token-interceptor.service";
+
+import { ROUTES } from "./app.routes";
+
+const routes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "signin", component: SignInComponent },
+  { path: "signup", component: SignUpComponent },
+  { path: "aboutus", component: AboutUsComponent },
+  { path: "terms", component: TermsComponent },
+  {
+    path: "subaccount",
+    component: SubaccountComponent,
+    canActivate: [UserGuard],
+  },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
     NavBarComponent,
     JumbotronComponent,
     FooterComponent,
@@ -60,37 +57,32 @@ import { PeliculaImagenPipe } from './pipes/pelicula-imagen.pipe';
     HomeComponent,
     AboutUsComponent,
     TermsComponent,
-    SubaccountComponent
-  ],
-  imports: [
-    BrowserModule,
-    RouterModule.forRoot(routes),
-    FormsModule,
-    HttpClientModule
-  ],
-  providers: [
-    UserGuard,
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: TokenInterceptorService,
-      multi: true
-    }
-=======
+    SubaccountComponent,
     HomeComponent,
     SearchComponent,
     LoadingComponent,
     PeliculaComponent,
     TarjetasComponent,
     NavbarComponent,
-    PeliculaImagenPipe
+    PeliculaImagenPipe,
   ],
   imports: [
     BrowserModule,
+    // RouterModule.forRoot(routes),
+    RouterModule.forRoot(ROUTES, { useHash: true }),
+    FormsModule,
     HttpClientModule,
     HttpClientJsonpModule,
-    RouterModule.forRoot(ROUTES, { useHash: true })
->>>>>>> sim
   ],
-  bootstrap: [AppComponent]
+  providers: [
+    UserGuard,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptorService,
+      multi: true,
+    },
+  ],
+
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
